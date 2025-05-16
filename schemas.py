ee13af2812a22978b5cf2dc297929affaaf28cc1
nm23aax <nm23aax@herts.ac.uk>
2025-05-16 17:16:28 +0100

    Change schema to fit new expected output

--- schemas.py
+++ schemas.py
@@ -3,5 +3,8 @@ from marshmallow import Schema, fields
 class TextInputSchema(Schema):
     text = fields.Str(required=True, validate=lambda s: len(s) > 0)
 
-class BooleanOutputSchema(Schema):
-    boolean = fields.Boolean(required=True)
+class TextOutputSchema(Schema):
+    text = fields.Str(
+        required=True,
+        validate=lambda s: s in ['Positive' 'Neutral' 'Negative' 'Irrelevant']
+    )
