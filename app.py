b812c50e471637d24a93f5515200f3709c78e1f9
nm23aax <nm23aax@herts.ac.uk>
2025-05-16 17:39:48 +0100

    Replace dummy fx with ML prediction

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_smorest import Api, Blueprint, abort
-from schemas import TextInputSchema, BooleanOutputSchema
+from schemas import TextInputSchema, TextOutputSchema
+import joblib
 
 app = Flask(__name__)
 
@@ -13,14 +14,21 @@ app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-
 
 api = Api(app)
 
+model = joblib.load("ml_data/twitter_training.pkl")
+
 mlTextSent_bp = Blueprint('mlTextSent_bp', __name__, url_prefix='/api')
 
 @mlTextSent_bp.route("/text-sentiment", methods=["POST"])
 @mlTextSent_bp.arguments(TextInputSchema)
-@mlTextSent_bp.response(200, BooleanOutputSchema)
+@mlTextSent_bp.response(200, TextOutputSchema)
 def check_sentiment(payload):
-    # do stuff
-    return {"boolean": False}
+    text = payload["text"]
+    result = model.predict([text])[0]
+    valid_outputs = ['Positive', 'Neutral', 'Negative', 'Irrelevant']
+    if result not in valid_outputs:
+        raise ValueError(f"{result} is not a valid sentiment value")
+    else:
+        return {"result": result}
 
 api.register_blueprint(mlTextSent_bp)
 
