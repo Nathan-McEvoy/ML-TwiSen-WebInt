0a31fed9781e5bf209b790a5a4b77f123c40dbac
nm23aax <nm23aax@herts.ac.uk>
2025-05-17 12:20:08 +0100

    Add test dataframe, clean text, optimise for panda Series

--- sentiment_analyser.py
+++ sentiment_analyser.py
@@ -1,31 +1,44 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 from sklearn.pipeline import Pipeline
 import joblib
-import math
+import text_preprocessing
+
+def clean_df_txt(dataframe):
+    try:
+        dataframe['text'] = text_preprocessing.clean_data(dataframe['text'])
+    except Exception as e:
+        print(f"Error cleaning text column {e}")
+        dataframe['text'] = None
+
+    dataframe.dropna(subset=['text'], inplace=True)
+    return dataframe
 
 cols = ['batch', 'game', 'class', 'text']
 data = pd.read_csv("ml_data/twitter_training.csv", names=cols)
-data.reset_index()
+data.reset_index(drop=True, inplace=True)
+test_data = pd.read_csv("ml_data/twitter_validation.csv", names=cols)
+test_data.reset_index()
 
-# scrub nan values from dataset
-for index, row in data.iterrows():
-    if isinstance(row['text'], str):
-        continue
-    if math.isnan(row['text']):
-        data.drop(index, inplace=True)
+data = clean_df_txt(data)
+test_data = clean_df_txt(test_data)
 
 X = data['text']
 y = data['class']
-
+X_t = test_data['text']
+y_t = test_data['class']
 
 pipeline = Pipeline([
-    ('tfidf', TfidfVectorizer(max_features=5000, stop_words='english')),
-    ('clf', LogisticRegression(multi_class='multinomial', max_iter=1000))
+    ('tfidf', TfidfVectorizer(max_features=10000, ngram_range=(1, 2))),
+    ('clf', LogisticRegression(max_iter=1000))
 ])
 
 pipeline.fit(X, y)
 
-joblib.dump(pipeline, 'ml_data/twitter_training.pkl')
+y_pred = pipeline.predict(X_t)
+print(classification_report(y_t, y_pred))
+
+# joblib.dump(pipeline, 'ml_data/twitter_training.pkl')
 print("Training Complete")
